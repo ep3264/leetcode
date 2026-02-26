feaf6a101b62196856cde53e79ebe087c9336dfc
Eugene Pavlenko <ep.git6@gmail.com>
2026-02-26 21:29:37 +0200

    CountingBits

--- Edu/regex.js
+++ Edu/regex.js
@@ -1,3 +1,5 @@
+import { log } from './include.js'
+
 let p = /(?<name1>a+?)(?<name2>b)/;
 let s = "aaab";
 ;
@@ -8,3 +10,27 @@ console.log(p.test(s));
 console.log(p2.test("Alo2"));
 console.log(p2.test("A2"));
 
+const s2 = `  f `;
+const p3 = /\s{1}/;
+log(s2.match(p3));
+log(p3.test(s2));
+
+
+const text = "cat scatter caterpillar cat.";
+
+console.log(text.match(/\bcat\b/g));
+// ["cat", "cat"]
+
+const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+console.log(re.test("Abc12345")); // true
+console.log(re.test("abc12345")); // false (no uppercase)
+
+const text2 = 'cat "cat" dog cat';
+
+console.log(text2.match(/cat(?=(?:[^"]*"[^"]*")*[^"]*$)/g));
+// ["cat", "cat"]
+
+
+const text3 = `abc`;
+console.log(text3.match(/(?=.*[c])abc/));
