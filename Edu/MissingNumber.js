a5e7ce0b9e9b3635bf6ad803bf4c76c9ca402b05
Eugene Pavlenko <ep.git6@gmail.com>
2026-02-28 22:35:23 +0200

    MissingNumber

---
+++ Edu/MissingNumber.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        let n = nums.length;
+
+        const sum = (n * (n + 1)) / 2;
+        const actualSum = nums.reduce((p, c) => p + c);
+
+        return sum - actualSum;
+    }
+}
+
+
+const sol = new Solution;
+
+console.log(sol.missingNumber([1, 2, 3]));
