de5b0d1226386ff1006ad6b5ea0a4ee8dd10b6b3
Eugene Pavlenko <ep.git6@gmail.com>
2026-03-09 00:37:50 +0200

    Cleaner solution

--- Edu/Best Time to Buy and Sell Stock_Cleaner.js
+++ Edu/Best Time to Buy and Sell Stock_Cleaner.js
@@ -4,29 +4,15 @@ class Solution {
      * @return {number}
      */
     maxProfit(prices) {
-        if (!prices.length || prices.length === 1) return 0;
-        let ret = 0;
-        let max = prices[0];
+        let profit = 0;
         let min = prices[0];
-        for (let i = 1; i < prices.length; i++) {
-
-            if (prices[i] > max) max = prices[i];
-
-            if (prices[i] < min) {
-                let diff = min - prices[i];
-                min = prices[i];
-                max -= diff;
-            }
-            if (max === min) continue;
-            if (max > prices[i]) continue;
-
-            if (ret < prices[i] - min) {
-                ret = prices[i] - min;
-            }
+        for (const price of prices) {
+            profit = Math.max(profit, price - min);
+            min = Math.min(min, price);
 
         }
 
-        return ret;
+        return profit;
     }
 }
 
