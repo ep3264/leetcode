6d58f3d5e3dfe3c76d3960fe7f70f5a856367a63
Eugene Pavlenko <ep.git6@gmail.com>
2026-03-11 00:14:48 +0200

    3Sum

--- Edu/3Sum.js
+++ Edu/3Sum.js
@@ -8,47 +8,51 @@ class Solution {
         // then use two pointer to find 3 suitable elements
 
         nums.sort((a, b) => a - b);
-        const indicies = new Set();
-        let left = 0, right = nums.length - 1;
-        const ret = [];
-        while (left != right) {
-            const diff = nums[left] + nums[right];
-            if (diff < 0) {
-                let thridIndex = right - 1;
-                const prevLength = ret.length;
-                while (thridIndex > left) {
-                    if (diff + nums[thridIndex] === 0 && !indicies.has(thridIndex)) {
-                        ret.push([nums[left], nums[thridIndex], nums[right]]);
-                        indicies.add(thridIndex);
-                        left++;
-                        right--;
-                        break;
-                    }
-                    thridIndex--;
-                }
-                if (prevLength === ret.length) left++;
-            } else {
-                const prevLength = ret.length;
-                let thridIndex = left + 1;
-                while (thridIndex < right) {
-                    if (diff + nums[thridIndex] === 0 && !indicies.has(thridIndex)) {
-                        ret.push([nums[left], nums[thridIndex], nums[right]]);
-                        indicies.add(thridIndex);
-                        left++;
-                        right--;
-                        break;
-                    }
-                    thridIndex++
-                }
-                if (prevLength === ret.length) right--;
-            }
+        const set = new Set();
 
+        const ret = [];
+        for (let i = 0; i < nums.length - 2; i++) {
+            let left = i, right = nums.length - 1;
+            while (left != right) {
+                const diff = nums[left] + nums[right];
+                if (diff < 0) {
+                    let thridIndex = right - 1;
+                    const prevLength = ret.length;
+                    while (thridIndex > left) {
+                        const p = [nums[left], nums[thridIndex], nums[right]];
+                        if (diff + nums[thridIndex] === 0 && !set.has(p.join(''))) {
+                            set.add(p.join(''));
+                            ret.push(p);
+
+
+                            break;
+                        }
+                        thridIndex--;
+                    }
+                } else {
+                    const prevLength = ret.length;
+                    let thridIndex = left + 1;
+                    while (thridIndex < right) {
+                        const p = [nums[left], nums[thridIndex], nums[right]];
+                        if (diff + nums[thridIndex] === 0 && !set.has(p.join(''))) {
+                            ret.push(p);
+                            set.add(p.join(''));
+
+                            break;
+                        }
+                        thridIndex++
+                    }
+
+                }
+                right--;
+            }
         }
 
-        return ret;
 
+        return ret;
     }
 }
 
 
-console.log(new Solution().threeSum([-1, 0, 1, 2, -1, -4]));
+
+console.log(new Solution().threeSum([0, 0, 0, 0]));
