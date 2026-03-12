8efb4425ae7b4e771577f10e96bc4b6b4ccfbf09
Eugene Pavlenko <ep.git6@gmail.com>
2026-03-12 17:59:31 +0200

    lowestCommonAncestor

---
+++ Edu/BT_LowestCommonAncestor.js
@@ -0,0 +1,75 @@
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+const root = new TreeNode(
+    6,
+    new TreeNode(
+        2,
+        new TreeNode(0),
+        new TreeNode(4,
+            new TreeNode(3),
+            new TreeNode(5)
+        )
+    ),
+    new TreeNode(
+        8,
+        new TreeNode(7),
+        new TreeNode(9)
+    )
+);
+
+
+const root2 = new TreeNode(
+    3,
+    new TreeNode(1,
+        null,
+        new TreeNode(2)
+
+    ),
+    new TreeNode(4)
+);
+
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) {
+        return this.recur(root, p, q)[0];
+    }
+
+    recur(root, p, q) {
+
+        if (!root) return [null, false];
+
+        const [left, lflag] = this.recur(root.left, p, q);
+        const [right, rflag] = this.recur(root.right, p, q);
+
+        if (lflag) return [left, true];
+        if (rflag) return [right, true];
+
+        if (left !== null && right !== null) return [root, true];
+
+        if (left !== null || right !== null) {
+            if (root.val === p.val || root.val === q.val) return [root, true];
+            else return left !== null ? [left, false] : [right, false];
+        }
+
+        if (root.val === p.val) return [root, false];
+        if (root.val === q.val) return [root, false];
+
+        return [null, false];
+    }
+}
+
+
+//console.log(new Solution().lowestCommonAncestor(root, new TreeNode(2), new TreeNode(4)).val);
+console.log(new Solution().lowestCommonAncestor(root2, new TreeNode(2), new TreeNode(4)));
