da24ccdef76038582ccd04b1a989832b1356d749
Eugene Pavlenko <ep.git6@gmail.com>
2026-03-26 01:24:57 +0200

    MissingNumber: Fix possible overflow.

--- java/src/com/leetcode/MissingNumber.java
+++ java/src/com/leetcode/MissingNumber.java
@@ -1,19 +1,21 @@
 package com.leetcode;
 
-import java.util.Arrays;
-
 public class MissingNumber {
     public static void main(String[] args) {
         System.out.println(
-                new MissingNumber().missingNumber(new int[]{0, 2})
+                new MissingNumber().missingNumber(new int[]{0, 2, 3, 4})
         );
+
     }
 
     public int missingNumber(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
+
         int n = nums.length;
-        int expected = n * (n + 1) / 2;
-        int ret = expected - Arrays.stream(nums).sum();
+        int ret = n;
+        for (int i = 0; i < n; i++) {
+            ret ^=  i ^ nums[i];
+        }
 
         return ret;
     }
