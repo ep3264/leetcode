d1a84b810c67d83fed9bc2b879be977c2046bcc0
Eugene Pavlenko <ep.git6@gmail.com>
2026-03-28 02:10:01 +0200

    CloneGraph DFS

--- java/src/com/leetcode/graph/CloneGraph.java
+++ java/src/com/leetcode/graph/CloneGraph.java
@@ -1,48 +1,47 @@
 package com.leetcode.graph;
+
 import java.util.HashMap;
 
 import static com.leetcode.graph.Node.buildGraph;
 
 public class CloneGraph {
     public static void main(String[] args) {
+        var graph = new int[][]{{2}, {1, 3}, {2}};
         Node cloned = new CloneGraph().cloneGraph(
                 buildGraph(
-                     new int[][] {{2}, {1, 3}, {2}}
+                       graph
                 )
         );
 
         System.out.println(
-               cloned
+                cloned
         );
     }
 
     public Node cloneGraph(Node node) {
-        if (node == null) return null;
+        var clonedByNode = new HashMap<Integer, Node>();
 
-        var map = new HashMap<Integer, Node>();
-        buildMap(node, map);
-
-        Node[] nodes = new Node[map.entrySet().size() + 1];
-        for (var entry : map.entrySet()) {
-            nodes[entry.getKey()] = new Node(entry.getKey());
-        }
-        for (var curNode : map.values()) {
-            var cloned = nodes[curNode.val];
-           for (var neighbor : curNode.neighbors) {
-               cloned.neighbors.add(nodes[neighbor.val]);
-           }
-        }
-
-        return nodes[1];
+        return dfs(node, clonedByNode);
     }
 
-    private void buildMap(Node node, HashMap<Integer, Node> map) {
-        map.put(node.val, node);
-        for (var neighbor : node.neighbors) {
-            if (!map.containsKey(neighbor.val)) {
-                map.put(neighbor.val, neighbor);
-                buildMap(neighbor, map);
-            }
+    public Node cloneGraphBFS(Node node) {
+        return null;
+    }
+
+    private Node dfs(Node node, HashMap<Integer, Node> clonedByMap) {
+        if (node == null) return null;
+
+        if (clonedByMap.containsKey(node.val)) {
+            return clonedByMap.get(node.val);
         }
+
+        var cloned = new Node(node.val);
+        clonedByMap.put(node.val, cloned);
+
+        for (var neighbor : node.neighbors) {
+            cloned.neighbors.add(dfs(neighbor, clonedByMap));
+        }
+
+        return cloned;
     }
 }
