ee2598a06b0bc70ceab72bc87dd518d58df66231
Eugene Pavlenko <ep.git6@gmail.com>
2026-03-28 04:57:20 +0200

    TopKFrequentElements Min-Heap

--- java/src/com/leetcode/arrays/TopKFrequentElements.java
+++ java/src/com/leetcode/arrays/TopKFrequentElements.java
@@ -1,12 +1,40 @@
 package com.leetcode.arrays;
 
+import java.util.*;
+
 public class TopKFrequentElements {
     public static void main(String[] args) {
-
+        System.out.println(
+                Arrays.toString(
+                        new TopKFrequentElements()
+                                .topKFrequent(
+                                        new int[]{1, 2, 1, 2, 1, 2, 3, 1, 3, 2},
+                                        2)
+                )
+        );
     }
 
     public int[] topKFrequent(int[] nums, int k) {
+        if (nums.length == 0) return new int[]{};
+        var freq = new HashMap<Integer, Integer>();
 
-        return null;
+        for (int i = 0; i < nums.length; i++) {
+                freq.merge(nums[i], 1, Integer::sum);
+         }
+
+
+        var queue = new PriorityQueue<Map.Entry<Integer,Integer>>(Comparator.comparingInt(Map.Entry::getValue));
+        for (var entry : freq.entrySet()) {
+            if (queue.size() < k) {
+                queue.offer(entry);
+            } else {
+                if (entry.getValue() > queue.peek().getValue()) {
+                    queue.poll();
+                    queue.offer(entry);
+                }
+            }
+        }
+
+        return queue.stream().mapToInt(e -> e.getKey().intValue()).toArray();
     }
 }
