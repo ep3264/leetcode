c872d237aec264923e87a79bb5a3a151a772c182
Eugene Pavlenko <ep.git6@gmail.com>
2026-04-24 22:27:53 +0300

    ClimbingStairs

---
+++ java/src/com/leetcode/refresh/ClimbingStairs.java
@@ -0,0 +1,23 @@
+package com.leetcode.refresh;
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        System.out.println(new ClimbingStairs().climbStairs(3));
+    }
+
+    public int climbStairs(int n) {
+        if (n == 1)  return 1;
+        int[] result = new int[n + 1];
+
+        result[1] = 1;
+        result[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+
+
+        return result[n];
+    }
+}
