9a0c93cb8de2bb717933b3c9a348e3b21535747f
Eugene Pavlenko <ep.git6@gmail.com>
2026-05-01 23:10:00 +0300

    ThreeSum

---
+++ java/src/com/leetcode/refresh/ThreeSum.java
@@ -0,0 +1,39 @@
+package com.leetcode.refresh;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        System.out.println(new ThreeSum().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        var result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum > 0) right--;
+                else left++;
+            }
+        }
+
+        return result;
+    }
+}
