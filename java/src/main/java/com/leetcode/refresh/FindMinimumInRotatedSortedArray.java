3dd702c459cf5d0b921b902dc928afc8aaecc691
Eugene Pavlenko <ep.git6@gmail.com>
2026-05-06 23:44:03 +0300

    ValidParentheses

--- java/src/main/java/com/leetcode/refresh/FindMinimumInRotatedSortedArray.java
+++ java/src/main/java/com/leetcode/refresh/FindMinimumInRotatedSortedArray.java
@@ -3,11 +3,9 @@ package com.leetcode.refresh;
 public class FindMinimumInRotatedSortedArray {
 
     public static void main(String[] args) {
-
         System.out.println(
                 new FindMinimumInRotatedSortedArray().findMin(new int[]{9, 1, 2, 3, 4, 5, 6})
         );
-
 
         System.out.println(
                 new FindMinimumInRotatedSortedArray().findMin(new int[]{2, 1})
@@ -18,21 +16,17 @@ public class FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
         int l = 0;
         int r = nums.length - 1;
-        while (l <= r) {
+        while (l < r) {
             if (nums[l] < nums[r]) return nums[l];
-
             int m = (int) (l + (r - l) / 2.0);
 
             if (nums[m] > nums[r]) {
                 l = m + 1;
-            } else if (nums[m] < nums[r]) {
-                r = m - 1;
             } else {
-                return nums[m];
+                r = m;
             }
         }
 
         return nums[l];
     }
-
 }
---
+++ java/src/main/java/com/leetcode/refresh/ValidParentheses.java
@@ -0,0 +1,35 @@
+package com.leetcode.refresh;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(new ValidParentheses().isValid("{}{[]}()(())"));
+    }
+
+    public boolean isValid(String s) {
+        var st = new int[s.length() + 1];
+        int pointer = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{')
+                st[++pointer] = c;
+
+            else {
+                if (pointer == 0)
+                    return false;
+
+                if ((c == ')' && st[pointer] == '(') ||
+                        (c == '}' && st[pointer] == '{') ||
+                        (c == ']' && st[pointer] == '[')) {
+                    pointer--;
+
+                } else
+                    return false;
+            }
+        }
+        return pointer == 0;
+    }
+}
