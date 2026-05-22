cfe8bd1dbf96a7800a4c3b57df554c3f5491c168
Eugene Pavlenko <ep.git6@gmail.com>
2026-05-22 23:14:00 +0300

    CloneGraph

---
+++ java/src/main/java/com/leetcode/refresh/CloneGraph.java
@@ -0,0 +1,50 @@
+package com.leetcode.refresh;
+
+import com.leetcode.graph.Node;
+
+import java.util.HashMap;
+
+import static com.leetcode.graph.Node.buildGraph;
+
+public class CloneGraph {
+    public static void main(String[] args) {
+        var graph = new int[][]{{2}, {1, 3}, {2}};
+        Node cloned = new CloneGraph().cloneGraph(
+                buildGraph(
+                       graph
+                )
+        );
+
+        System.out.println(
+                cloned
+        );
+    }
+
+    public Node cloneGraph(Node node) {
+        var clonedByNode = new HashMap<Integer, Node>();
+
+        return dfs(node, clonedByNode);
+    }
+
+    // TODO:
+    public Node cloneGraphBFS(Node node) {
+        return null;
+    }
+
+    private Node dfs(Node node, HashMap<Integer, Node> clonedByMap) {
+        if (node == null) return null;
+
+        if (clonedByMap.containsKey(node.val)) {
+            return clonedByMap.get(node.val);
+        }
+
+        var cloned = new Node(node.val);
+        clonedByMap.put(node.val, cloned);
+
+        for (var neighbor : node.neighbors) {
+            cloned.neighbors.add(dfs(neighbor, clonedByMap));
+        }
+
+        return cloned;
+    }
+}
